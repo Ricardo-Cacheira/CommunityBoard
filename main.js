e897c6a5fc06cdfcd4a709212166ae126811c895
Ricardo Cacheira <ricardo.cacheira@hotmail.com>
2018-04-20 16:31:40 +0100

    Form and send

--- main.js
+++ main.js
@@ -4,3 +4,22 @@ $(function(){
         $(this).addClass('active');
      });
  });
+
+ var but = document.getElementById("submit");
+
+ but.onclick()
+ {
+    var user = document.getElementById("username");
+    var pass = document.getElementById("password");
+    var email = document.getElementById("email");
+    var fname = document.getElementById("firstname");
+    var lname = document.getElementById("lastname");
+    var birth = document.getElementById("birthday");
+
+    $.getJSON('/insertUser/'+user+'/'+pass+'/'+email+'/'+fname+'/'+lname+'/'+birth, end);
+
+    function end(data)
+    {
+        console.log(data);
+    }
+ }
